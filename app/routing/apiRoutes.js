d49bbb14df15ac23c4da53e5955d661baad76ea1
ScottTAnderson <42193132+ScottTAnderson@users.noreply.github.com>
2019-03-14 01:09:59 -0500

    added data validation and reset forms

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,24 @@
 var friends = require("../data/friends");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    app.get("/api/friends", function(req, res) {
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
 
     app.post("/api/friends", function (req, res) {
+        var differenceArray = [];
+        var numberOfFriends = friends.length;
         friends.push(req.body);
-        // res.json(false);
+        for (i = 0; i < numberOfFriends; i++) {
+            var totalDifference = 0;
+            for (j = 0; j < 10; j++) {
+                totalDifference += Math.abs(friends[numberOfFriends].scores[j] - friends[i].scores[j]);
+            }
+            differenceArray.push(totalDifference);
+        }
+        var closestMatch = Math.min(...differenceArray);
+        var position = differenceArray.indexOf(closestMatch)
+        res.json(friends[position]);
     });
 };
